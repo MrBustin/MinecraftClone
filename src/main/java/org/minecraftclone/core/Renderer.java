9477e540645b8c5871c873319d79c0c766edb1b7
MrBustin <redlegoboy1@gmail.com>
2026-03-02 23:58:06 -0500

    Face Culling

--- src/main/java/org/minecraftclone/core/Renderer.java
+++ src/main/java/org/minecraftclone/core/Renderer.java
@@ -70,6 +70,7 @@ public class Renderer {
 
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        glEnable(GL_CULL_FACE);
 
         shader = new ShaderProgram(VERT, FRAG);
         texture = new Texture("/textures/atlas.png");
